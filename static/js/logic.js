73b6a470cc5b447b0ecd24b62bf5eb3c48e7a7b5
maali007 <maali007@umn.edu>
2021-06-18 00:35:23 -0500

    Added earthquakes layerGroup.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-//Step 1: Create the tile layer that will be the background of our map
+// Step 1: Create the tile layer that will be the background of our map
 var mbLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -7,3 +7,6 @@ var mbLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   id: "mapbox/light-v10",
   accessToken: API_KEY
 });
+
+// Step 2: Create earthquake layerGroup
+var eQuakes = L.layerGroup();
