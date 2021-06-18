716c867388f92bf7b6a473e3133dcb468ab7ed65
maali007 <maali007@umn.edu>
2021-06-18 00:30:07 -0500

    Added tile layer.

---
+++ static/js/logic.js
@@ -0,0 +1,9 @@
+//Step 1: Create the tile layer that will be the background of our map
+var mbLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+});
